d0c3c8e3472b50851aaa05c730e5e774e5c4a09f
Salvador Babinet <salvador.babinet@mail.mcgill.ca>
2024-05-15 17:49:40 -0400

    Relatively tidy data import

--- 01-AusStoich-EDA.R
+++ 01-AusStoich-EDA.R
@@ -8,13 +8,14 @@ raw_data #For reference
 
 tidy_data <- read_csv(
   file = 'austraits_leaf_stoichiometry_MASTER_v1.0_10-05-2024.csv',
+  na = c('', 'NA', 'uncertain'),
   col_types = cols(
     woodiness = col_factor(c('herbaceous', 'woody')),
-    reclass_life_history = col_factor(c('short', 'long'))
+    reclass_life_history = col_factor(c('short', 'long')),
+    putative_BNF = col_factor(c('0', '1')),
+    myc_type = col_factor(c('AM', 'EcM', 'EcM-AM', 'ErM', 'NM', 'NM-AM'))
     )
   )
+tidy_data <- tidy_data |> select(Unique_ID:CP_ratio)
 tidy_data
 
-# Post-import data tidying 
-glimpse(raw_data)
-raw_data |> count(myc_type)
